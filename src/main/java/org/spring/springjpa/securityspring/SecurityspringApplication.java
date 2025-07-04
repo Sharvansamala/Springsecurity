4a99559f6e02e7146c3c2d40cd52d7a9c58134b8
Sharvansamala <sharvansamala2002@gmail.com>
2025-07-03 19:10:25 -0500

    Dot env

--- src/main/java/org/spring/springjpa/securityspring/SecurityspringApplication.java
+++ src/main/java/org/spring/springjpa/securityspring/SecurityspringApplication.java
@@ -1,12 +1,17 @@
 package org.spring.springjpa.securityspring;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  SecurityspringApplication {
+public class SecurityspringApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().load(); // Load environment variables from .env file
+
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(SecurityspringApplication.class, args);
     }
 
